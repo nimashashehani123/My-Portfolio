e9a73beea119fa7ca22cc3bba3d94e4897568f80
T.L. Nimasha Shehani <nimashashehani0715@gmail.com>
2025-08-29 10:25:53 +0530

    add horizontal scroll container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-function App() {
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
 
-  return (
-    <h1>Portfolio</h1>
-  )
+
+const App: React.FC = () => {
+    return (
+        <div className="relative w-screen h-screen overflow-x-auto flex snap-x snap-mandatory scroll-smooth">
+            <Navbar />
+            <section className="min-w-screen h-screen snap-start"><Hero /></section>
+            <section className="min-w-screen h-screen snap-start"><About /></section>
+            <section className="min-w-screen h-screen snap-start"><Skills /></section>
+            <section className="min-w-screen h-screen snap-start"><Projects /></section>
+            <section className="min-w-screen h-screen snap-start"><Contact /></section>
+        </div>
+    );
 }
 
-export default App
+export default App;
